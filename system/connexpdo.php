ac2cd03121a91e249902de5859fcc01bd885f2af
ath140 <athmohamed140gmail.com>
2025-01-28 09:00:10 +0100

    system 01

---
+++ system/connexpdo.php
@@ -0,0 +1,12 @@
+<?php
+include_once("define.php");
+function connexpdo($base){
+    $root = USER;
+    try{
+      $local ="mysql:host=".HOST.";dbname=".$base;
+      $dns = new PDO($local,$root,'');
+    }catch(PDOException $e){
+      die("connection impossible").$e->getMessage();
+    }
+} 
+?>
